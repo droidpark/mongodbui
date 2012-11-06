944bc7604643f06b8aa6f936957a4b990313672c
kamilors <kamilors@gmail.com>
2012-11-06 14:48:52 +0200

    added log4j support.

--- mongoui-fx/src/main/java/com/droidpark/mongoui/App.java
+++ mongoui-fx/src/main/java/com/droidpark/mongoui/App.java
@@ -1,5 +1,7 @@
 package com.droidpark.mongoui;
 
+import org.apache.log4j.Logger;
+
 import com.droidpark.mongoui.form.MainForm;
 import com.droidpark.mongoui.util.Language;
 
@@ -12,8 +14,13 @@ import javafx.application.Application;
  */
 
 public class App {
+	
+	private static Logger logger = Logger.getLogger(App.class);
+	
     public static void main( String[] args ) {
+    	logger.info("MongoUI initializing...");
     	Language.init();
+    	logger.info("MongoUI initilazed.");
     	Application.launch(MainForm.class, args);
     }
 }
